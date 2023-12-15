bf86ccc11e6594059c3ef143d730ce54de633e9e
wolfytien888 <wolfytien888@gmail.com>
2023-12-15 18:34:49 +0800

    replace files

--- iris_prediction.py
+++ iris_prediction.py
@@ -10,7 +10,7 @@ sepal_length = st.slider('花萼長度:', min_value=3.0, max_value=8.0, value=5.
 sepal_width = st.slider('花萼寬度:', min_value=2.0, max_value=5.0, value=3.5)
 petal_length = st.slider('花瓣長度:', min_value=1.0, max_value=7.0, value=4.4)
 petal_width = st.slider('花瓣寬度:', min_value=0.1, max_value=2.5, value=1.3)
-# 說明
+
 labels = ['setosa', 'versicolor', 'virginica']
 if st.button('預測'):
     X_new = [[sepal_length,sepal_width,petal_length,petal_width]]
